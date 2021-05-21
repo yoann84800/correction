ca8eb08bfe14dd260a90db10535d08ecd0044ded
yoann84800 <yoann.biliato@hotmail.fr>
2021-05-21 10:56:20 +0200

    exercise2

---
+++ js/js.js
@@ -0,0 +1,28 @@
+let = livre1 = {
+    réference : "yoyo";
+    titre: "les trixe";
+    auteur: "blablou";
+}
+let = livre2 = {
+reference : "ann";
+titre:"toutou";
+auteur:"babou";
+
+}
+ 
+let = livre3 = {
+Reference: "456";
+titre: "maman";
+ateur: "lolilop";
+}
+let = tab = length {
+}
+var livre =new Array();{
+livre1["yoyo ,les trixe ,blalou"] = livre;
+lvre2["ann, toutou, babou"] = livre;
+livre3["456, maman, lolilop"] = livre;
+livre[3]= livre1, livre2, livre3;
+}
+
+
+
